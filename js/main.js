674b0e37078bf76239e9cd728141db9b07db8006
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-04-19 18:12:35 +0000

    Ensure page is mobile-responsive

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ var $start = document.querySelector('.start-button');
 var $header = document.querySelector('header');
 var $agentScreen = document.querySelector('.agents-screen');
 var $mainScreen = document.querySelector('.main-screen');
-var $agentList = document.querySelector('#agent-list');
+var $agentList = document.querySelector('.agent-list');
+var $agentsButton = document.querySelector('.agents-link');
 
 $start.addEventListener('click', handleStart);
+$agentsButton.addEventListener('click', handleStart);
 function handleStart(event) {
   $header.className = 'header';
   $agentScreen.className = 'agents-screen';
@@ -15,16 +17,14 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://valorant-api.com/v1/agents');
 xhr.responseType = 'json';
 xhr.addEventListener('load', function () {
-  console.log('xhr.status:', xhr.status);
-  console.log('xhr.response:', xhr.response);
   for (var i = 0; i < xhr.response.data.length; i++) {
     if (xhr.response.data[i].isPlayableCharacter === true) {
-      var $iconHolder = document.createElement('div');
+      var $li = document.createElement('li');
       var $icon = document.createElement('img');
       $icon.setAttribute('class', 'agent-icon');
       $icon.setAttribute('src', xhr.response.data[i].displayIconSmall);
-      $iconHolder.appendChild($icon);
-      $agentList.appendChild($iconHolder);
+      $li.appendChild($icon);
+      $agentList.appendChild($li);
     }
   }
 });
